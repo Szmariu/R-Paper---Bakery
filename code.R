cddd7bcc195a6c8460810e9b7beae7e12f905ae7
Michał Szałański <szmariu.miki@gmail.com>
2019-03-20 16:47:19 +0100

    Removed bread, added disssimilarity

--- code.R
+++ code.R
@@ -16,11 +16,13 @@ cBlue = '#378CC7'
 
 
 # Read the data
-dataImport <- read.csv('data/BreadBasket_DMS.csv')
+dataImport <- read.csv('data/teaBasket_DMS.csv')
 
 # Remove transactions with NONE and Adjustment
 dataExport <- dataImport %>% filter(dataImport$Item != "NONE" & dataImport$Item != "Adjustment")
-dataExportNoCoffee <- dataImport %>% filter(dataImport$Item != "NONE" & dataImport$Item != "Adjustment" & dataImport$Item != "Coffee" & dataImport$Item != "Bread")
+
+# Remove two most frequent items 
+dataExportNoCoffee <- dataImport %>% filter(!(dataImport$Item %in% c("NONE", "Adjustment", "Coffee", "Bread", "Postcard")))
 
 # Write only the relevant columns
 write.csv(dataExport[, c(3:4)], './data/transactions.csv')
@@ -33,7 +35,7 @@ coffee <- read.transactions('data/transactions.csv',
                            sep = ',', 
                            header = TRUE)
 
-bread <- read.transactions('data/transactionsNoCoffee.csv', 
+tea <- read.transactions('data/transactionsNoCoffee.csv', 
                             format="single", 
                             cols = c('Transaction', 'Item'), 
                             sep = ',', 
@@ -42,46 +44,46 @@ bread <- read.transactions('data/transactionsNoCoffee.csv',
 # With coffee
 coffee
 
-# Without coffee
-bread
+# Without coffee and bread
+tea
 
 
 
 
 ############## Cross tables
-breadCount <- bread %>% crossTable(measure="count", sort=TRUE)
+teaCount <- tea %>% crossTable(measure="count", sort=TRUE)
 coffeeCount <- coffee %>% crossTable(measure="count", sort=TRUE)
 
 # Coffee in most of transactions
 coffeeCount[1,1]
-breadCount[1,1]
+teaCount[1,1]
 
-breadSupport <- bread %>% crossTable(measure="support", sort=TRUE)
-breadLift <- bread %>%crossTable(measure="lift", sort=TRUE)
-breadChi <- bread %>% crossTable(measure="chiSquared", sort=TRUE)
+teaSupport <- tea %>% crossTable(measure="support", sort=TRUE)
+teaLift <- tea %>%crossTable(measure="lift", sort=TRUE)
+teaChi <- tea %>% crossTable(measure="chiSquared", sort=TRUE)
 
 # p-value of test, H0:independent rows and columns
-breadCount[1:8,1:6]
-print(breadSupport[1:8,1:6], digits = 1)
-print(breadLift[1:8,1:6], digits = 2)
-print(breadChi[1:8,1:6], digits = 1)
+teaCount[1:8,1:6]
+print(teaSupport[1:8,1:6], digits = 1)
+print(teaLift[1:8,1:6], digits = 2)
+print(teaChi[1:8,1:6], digits = 1)
 
 ############# Eclat
 
 # Low support still gives items that have at least ~ 8 occurences
-breadFreqItems <- bread %>% eclat(list(supp=0.0003, maxlen=4)) 
-median(breadFreqItems@quality$count)
+teaFreqItems <- tea %>% eclat(list(supp=0.0003, maxlen=4)) 
+median(teaFreqItems@quality$count)
 
-breadFreqRules <- breadFreqItems %>% ruleInduction(bread, confidence=0.4)
-breadFreqRules
-inspect(breadFreqRules)
+teaFreqRules <- teaFreqItems %>% ruleInduction(tea, confidence=0.4)
+teaFreqRules
+inspect(teaFreqRules)
 
 
 
 ########### Apriori
-breadRules <- bread %>% apriori(list(supp=0.0004, conf=0.3)) %>% filter(Lift < 50)
+teaRules <- tea %>% apriori(list(supp=0.0004, conf=0.3)) %>% filter(Lift < 50)
 
-breadRules %>% 
+teaRules %>% 
   sort(by="lift", decreasing=TRUE) %>% 
   inspect()
 
@@ -96,11 +98,6 @@ coffee %>%
   sort(by="confidence", decreasing=TRUE) %>%
   head(15) %>%
   inspect(ruleSep = ">>>", itemSep = " + ", setStart = "", setEnd ="") 
-
-abcd <- coffee %>% 
-  apriori(list(supp=0.001,conf = 0.10),
-                  appearance=list(default="lhs", rhs="Coffee"),
-                  control=list(verbose=F))
 
 coffee %>% 
   apriori(list(supp=0.001,conf = 0.10),
@@ -133,27 +130,25 @@ coffee %>%
 coffee %>% itemFrequencyPlot(topN=15, type="absolute", main="Item Absolute Frequency", col = cYellow, border = NA) 
 coffee %>% itemFrequencyPlot(topN=15, type="relative", main="Item Relative Frequency", col = cBlue, border = NA) 
 
+tea %>% itemFrequencyPlot(topN=15, type="relative", main="Item Relative Frequency", col = cBlue, border = NA) 
+
 # A sample
 image(coffee[1000:1200])
-image(bread[1000:1200])
+image(tea[1000:1200])
 
-plot(breadFreqRules, method="matrix", measure="confidence")
-
-plot(breadFreqRules) 
-plot(breadFreqRules, measure=c("support","lift"), shading="confidence")
-
-plot(breadFreqRules, shading="order", control=list(main="Two-key plot"))
-
-plot(breadFreqRules, method="grouped") # this one has a great potential
-plot(breadFreqRules, method="graph") # this one too
-plot(breadFreqRules, method="graph", control=list(type="items"))
-plot(breadFreqRules, method="paracoord", control=list(reorder=TRUE))
+plot(teaFreqRules, method="matrix", measure="confidence")
+plot(teaFreqRules) 
+plot(teaFreqRules, measure=c("support","lift"), shading="confidence")
+plot(teaFreqRules, shading="order", control=list(main="Two-key plot"))
+plot(teaFreqRules, method="graph")
+plot(teaFreqRules, method="paracoord", control=list(reorder=TRUE))
 
 
 
 ############## Dissimilarity
-trans.sel<-coffee[,itemFrequency(coffee)>0.04] # selected transactions
-d.jac.i<-dissimilarity(trans.sel, which="items") # Jaccard by default
 
 # most are dissimilar - 90%
-round(d.jac.i,2) 
+coffee[,itemFrequency(coffee)>0.05] %>%
+  dissimilarity(which="items") %>%
+  round(2) 
+
